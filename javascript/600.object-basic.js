21156cf4d0db3a60f1fb8ad3e3a45113f790c159
hoojo_@126.com <hoojo_@126.com>
2025-03-12 11:53:15 +0800

    feat(object): 在 Person 构造函数中添加 new 关键字检查
    - 在 Person 构造函数中添加 new.target 检查
    - 如果没有使用 new 关键字调用函数，抛出错误
    - 这样做可以确保 Person 函数只能通过 new 关键字来创建实例

--- javascript/600.object-basic.js
+++ javascript/600.object-basic.js
@@ -23,6 +23,15 @@ console.log(person.getAge());  // 18
 // 构造函数，通过 new 可以多次创建对象
 // --------------------------------------------------------------------------
 function Person(name, age, sex) {
+    // 在没有 new 的情况下，new.target 为 undefined
+    console.log("Person constructor", new.target);
+    if (new.target === undefined) {
+        throw new Error("必须使用 new 创建实例");
+        
+        // 递归调用，创建实例并返回进行容错
+        // return new Person(name, age, sex);
+    }
+
     this.name = name;
     this.age = age;
     this.sex = sex;
@@ -37,3 +46,6 @@ let person1 = new Person("小明", 18, "男");
 let person2 = new Person("小明", 18, "男");
 console.log(person1);
 person1.sayHello(); // hello, my name is 小明
+
+// 没有使用 new 创建实例，会报错
+// Person("小明", 18, "男");
