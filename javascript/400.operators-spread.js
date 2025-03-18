fce7bd3f62c1b9bd6ffc30ea3821e107c5f9e119
hoojo_@126.com <hoojo_@126.com>
2025-03-18 11:09:32 +0800

    feat/javascript: 重命名文件并扩展对象展开功能
    - 将 400.object-spread.js 重命名为 400.operators-spread.js
    -增加了扩展运算符（spread operator）和剩余参数（rest parameters）的示例
    - 添加了函数调用、apply、call 和 bind 方法的相关示例
    - 新增 801.func-type.js 文件，介绍了函数的相关属性和方法

--- javascript/400.operators-spread.js
+++ javascript/400.operators-spread.js
@@ -1,6 +1,7 @@
 /****************************************************************************
- *  JavaScript 对象展开
+ *  JavaScript 对象展开，扩展运算符（spread operator）和剩余参数（rest parameters）
  *    - 使用 (...) 来克隆对象或将多个对象合并为一个
+ *    - 将可迭代对象（如数组、字符串等）或者对象展开为单独的元素
  ****************************************************************************/
 
 // ---------------------------------------------------------------------------
@@ -22,31 +23,52 @@ console.log(list);      // [3, 4]
 // 对象展开
 // ---------------------------------------------------------------------------
 const user = {
-  name: 'Jerry',
-  age: 18,
-  sex: 'male'
+    name: 'Jerry',
+    age: 18,
+    sex: 'male'
 };
 
 // 合并user属性值，创建新对象
 const newUser = {
-  ...user,
-  name: 'Tom',
-  age: 20,
-  address: 'beijing'
+    ...user,
+    name: 'Tom',
+    age: 20,
+    address: 'beijing'
 };
 console.log(newUser);  // { name: 'Tom', age: 20, sex: 'male', address: 'beijing' }
 
 const address = {
-  city: 'beijing',
-  street: 'dongcheng'
+    city: 'beijing',
+    street: 'dongcheng'
 };
 
 console.log({
-  ...user,
-  ...address
+    ...user,
+    ...address
 });   // { name: 'Jerry', age: 18, sex: 'male', city: 'beijing', street: 'dongcheng' }
 
 
 // 克隆对象，浅复制
 const cloneUser = {...user};
 console.log(cloneUser);
+
+
+// ---------------------------------------------------------------------------
+// 函数调用
+// ---------------------------------------------------------------------------
+const params = [1, 2, 3, 4, 5];
+function sum(...args) {
+    let result = 0;
+    for (let i = 0; i < args.length; i++) {
+        result += args[i];
+    }
+    return result;
+}
+
+console.log(sum(1, 2, 3, 4, 5));    // 15
+console.log(sum(...params));    // 15
+
+function add(a, b, c) {
+    return a + b + c;
+}
+console.log(add(...params));    // 6，会省略超出的长度的参数
