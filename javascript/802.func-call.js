86dd9f88b337acd7e0b3a047dd5e674852a86a61
hoojo_@126.com <hoojo_@126.com>
2025-03-18 14:49:04 +0800

    feat/javascript: 添加 Function.prototype.call 的使用示例
    - 添加了多个 call 方法的使用示例，包括：
      - 改变函数内部 this 的指向
      - 为对象链接构造函数
      - 调用原生方法
    - 通过具体代码展示了 call 方法的多种应用场景和用途

--- javascript/802.func-call.js
+++ javascript/802.func-call.js
@@ -34,3 +34,45 @@ console.log(add.call(null, 1, 2));  // 3
 // ----------------------------------------------------------------------------
 // this 的指向
 // ----------------------------------------------------------------------------
+let message = "javascript";
+let param = {
+    message: "hello world",
+};
+
+function say(user) {
+    console.log(`${user} say: ${this.message}`);
+}
+
+say.call(param, "Jack");    // Jack say: hello world
+say.call(this, "Tom");      // Tom say: javascript, this 指向全局对象
+
+
+// ----------------------------------------------------------------------------
+// call 方法 为对象链接构造函数
+// ----------------------------------------------------------------------------
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+function Student(name, age, grade) {
+    Person.call(this, name, age);
+    this.grade = grade;
+}
+
+const student = new Student("Jack", 18, 1);
+console.log(student); // Student { name: 'Jack', age: 18, grade: 1 }
+
+
+// ----------------------------------------------------------------------------
+// call 方法 调用原生方法
+// ----------------------------------------------------------------------------\
+function isOdd(num) {
+    return num % 2 === 1;
+}
+
+function filterNumber(...args) {
+    return Array.prototype.filter.call(args, isOdd);
+}
+
+console.log(filterNumber(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));   // [ 1, 3, 5, 7, 9 ]
