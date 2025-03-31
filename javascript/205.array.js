e804dbd302f0befbb7c04faf401b7559d9405290
hoojo_@126.com <hoojo_@126.com>
2025-03-31 10:55:54 +0800

    docs(javascript): 更新数组相关函数说明
    - 新增 toSorted() 和 fill() 函数的说明- 补充部分函数的示例代码
    - 优化文档结构，增加分节符
    -修正部分函数的描述信息

--- javascript/205.array.js
+++ javascript/205.array.js
@@ -23,6 +23,8 @@
  *          - every() – 检查数组中每个元素是否通过测试。
  *          - some() – 检查数组中至少有一个元素是否通过测试。
  *          - sort() – 对数组中的元素进行排序。
+ *          - toSorted() – 对数组进行排序，并返回一个新数组。
+ *          - fill() – 用一个值填充数组。
  *          - forEach() – 循环遍历数组元素。
  *      - 第 5 节：操作数组
  *          - concat() – 将两个数组合并为一个数组。
@@ -69,9 +71,10 @@ console.log(colors);    // [ 'red' ]
 colors.length = 3;
 console.log(colors);    // [ 'red', , ]
 
-
 colors.length = 0;
 console.log("--------------------------------------")
+
+
 // ----------------------------------------------------------------------------
 // 添加元素：push() 和 unshift()
 // ----------------------------------------------------------------------------
@@ -124,7 +127,7 @@ console.log(names); // [ 'jack', 'rose', 'lily', 'tom', 'jim', 'joe' ]
 
 console.log(names.join(" "));
 
-console.log("--------------------------------------")
+console.log("--------------------------------------");
 // 添加到最前面
 console.log(colors.unshift("white")); // 7
 console.log(colors); // [ 'white', 'yellow', 'black', 'white', 'gray', 'red', 'blue' ]
@@ -134,11 +137,246 @@ console.log(colors); // [ '#111', '#CCC', 'white', 'yellow', 'black', 'white', '
 
 // 添加数组到最前面
 colors.unshift(...arrays);
-console.log("--------------------------------------")
+console.log("--------------------------------------");
+
 
 // ----------------------------------------------------------------------------
 // 删除元素：pop() 和 shift()
 // ----------------------------------------------------------------------------
+colors = ["blue", "white", "yellow", "black"];
 // 删除末尾，弹出末尾元素
-console.log(colors.pop()); // blue
-console.log(colors);    // [ '#111', '#CCC', 'white', 'yellow', 'black', 'white', 'gray', 'red' ]
+console.log(colors.pop()); // black
+console.log(colors);    // [ 'blue', 'white', 'yellow' ]
+
+// 删除最前面，弹出最前面元素
+console.log(colors.shift()); // blue
+console.log(colors);    // [ 'white', 'yellow' ]
+console.log("--------------------------------------");
+
+
+// ----------------------------------------------------------------------------
+// 删除元素：splice()
+// ----------------------------------------------------------------------------
+colors = ["blue", "white", "yellow", "black"];
+console.log(colors.splice(1, 1)); // [ 'white' ]
+console.log(colors);    // [ 'blue', 'yellow', 'black' ]
+
+// 从第1个位置，删除1个，并插入2个
+console.log(colors.splice(1, 1, "#red", "#green")); // [ 'yellow' ]
+console.log(colors);    // [ 'blue', 'red', 'green', 'black' ]
+
+// 第1个位置插入元素
+console.log(colors.splice(1, 0, "#ins")); // []
+console.log(colors); // [ 'blue', '#ins', 'red', 'green', 'black' ]
+
+console.log(colors.splice(2, 2, "#ins"));   // [ 'green', 'black' ]
+console.log(colors); // [ 'blue', '#ins', 'red', '#ins' ]
+
+
+// ----------------------------------------------------------------------------
+// 复制元素：slice()
+// ----------------------------------------------------------------------------
+colors = ["blue", "white", "yellow", "black"];
+
+// 克隆数组
+let newColors = colors.slice();
+console.log({ newColors }); // [ 'blue', 'white', 'yellow', 'black' ]
+console.log(colors === newColors);  // false
+
+newColors.push("red");
+console.log({ colors });
+
+// 复制数组
+console.log(colors.slice(1, 3));
+
+console.log("--------------------------------------");
+function copyArray() {
+    return Array.prototype.slice.call(arguments);
+}
+console.log(copyArray(1, 2, 3, 4, 5));
+
+
+// ----------------------------------------------------------------------------
+// indexOf() 和 lastIndexOf()
+// ----------------------------------------------------------------------------
+colors = ["blue", "white", "yellow", "black", "blue"];
+console.log(colors.indexOf("blue"));    // 0
+console.log(colors.indexOf("#blue"));   // -1
+console.log(colors.lastIndexOf("blue"));    // 4
+
+
+// ----------------------------------------------------------------------------
+// includes() 和 find()
+// ----------------------------------------------------------------------------
+colors = ["blue", "white", "yellow", "black", "blue"];
+console.log(colors.includes("blue"));  // true
+console.log(colors.includes("#blue")); // false
+console.log(colors.includes("white", 2));  // false
+
+// 查找数组中第一个满足的元素
+console.log(colors.find(color => color === "blue"));  // blue
+console.log(colors.find(color => color === "#blue")); // undefined
+
+const context = {
+    name: "jack",
+    age: 18,
+    sex: "male"
+};
+colors.find(function (value, index, arrays) {
+    console.log({ value, index, arrays });
+
+    // 使用上下文时，不要使用箭头函数
+    console.log(this.name); // this 指向 context
+    return value === "blue";
+}, context);
+// {
+//   value: 'blue',
+//   index: 0,
+//   arrays: [ 'blue', 'white', 'yellow', 'black', 'blue' ]
+// }
+
+
+// ----------------------------------------------------------------------------
+// findIndex()，类似find的语法
+// ----------------------------------------------------------------------------
+colors = ["blue", "white", "yellow", "black", "blue"];
+console.log(colors.findIndex(color => color === "blue"));  // 0
+
+colors.findIndex(function (value, index, arrays) {
+    // 使用上下文时，不要使用箭头函数
+    console.log(this.name); // this 指向 context
+    return value === "blue";
+}, context);
+
+
+// ----------------------------------------------------------------------------
+// map 转换元素
+// ----------------------------------------------------------------------------
+colors = ["blue", "white", "yellow", "black", "blue"];
+console.log(colors.map(color => color.toUpperCase()));  // [ 'BLUE', 'WHITE', 'YELLOW', 'BLACK', 'BLUE' ]
+
+colors.map(function (value, index, arrays) {
+    return value.toUpperCase();
+}, context);
+
+
+// ----------------------------------------------------------------------------
+// filter 过滤元素
+// ----------------------------------------------------------------------------
+console.log(colors.filter(color => color === "blue"));  // [ 'blue', 'blue' ]
+colors.filter(function (value, index, arrays) {
+    return value === "blue";
+}, context);
+
+
+// ----------------------------------------------------------------------------
+// reduce 累加元素 & reduceRight
+// -----------------------------------------------------------------------------
+colors = ["a", "b", "c", "d", "e"];
+console.log(colors.reduce((pre, cur) => pre + cur.toUpperCase()));    // aBCDE
+// 从右边开始累加，倒序
+console.log(colors.reduceRight((pre, cur) => pre + cur.toUpperCase())); // eDCBA
+
+colors.reduce(function (pre, cur, index, arrays) {
+    console.log({ pre, cur, index, arrays });
+    return false;
+}); // { pre: 'a', cur: 'b', index: 1, arrays: [ 'a', 'b', 'c', 'd', 'e' ] }
+
+// 带初始值, 指定了 initialValue，则 callbackFn 函数将在第一次调用时将 previousValue 初始化为 initialValue，并将 currentValue 初始化为第一个数组元素。
+colors.reduce(function (pre, cur, index, arrays) {
+    console.log({ pre, cur, index, arrays });
+    return false;
+}, "X"); // { pre: 'X', cur: 'a', index: 1, arrays: [ 'a', 'b', 'c', 'd', 'e' ] }
+
+
+// ----------------------------------------------------------------------------
+// 判断是否满足条件：every() 和 some()
+// ----------------------------------------------------------------------------
+colors = ["blue", "white", "yellow", "black", "blue"];
+// 判断所有元素匹配条件
+console.log(colors.every(color => color === "blue"));  // false
+console.log(colors.every(color => color.length > 3));  // true
+
+// 判断至少有一个满足条件
+console.log(colors.some(color => color === "blue"));    // true
+console.log(colors.some(color => color === "Xxx"));     // false
+
+
+// ----------------------------------------------------------------------------
+// 填充元素 fill()
+// ----------------------------------------------------------------------------
+colors = new Array(5);
+console.log(colors.fill("1", 0, 3));    // [ '1', '1', '1', <2 empty items> ]
+console.log(colors);    // [ '1', '1', '1', <2 empty items> ]
+
+
+// ----------------------------------------------------------------------------
+// 数组排序： sort()
+// ----------------------------------------------------------------------------
+colors = ["blue", "white", "yellow", "black", "blue"];
+console.log(colors.sort()); // [ 'black', 'blue', 'blue', 'white', 'yellow' ]
+console.log(colors); // [ 'black', 'blue', 'blue', 'white', 'yellow' ]
+console.log(colors.sort((a, b) => {
+    if (a > b) return -1;
+    if (a < b) return 1;
+    return 0;
+})); // [ 'yellow', 'white', 'blue', 'blue', 'black' ]
+
+
+// ------------------------------------------------------------------------------
+// 数组排序： toSorted() 排序但不更改原始数组
+// ------------------------------------------------------------------------------
+colors = ["blue", "white", "yellow", "black", "blue"];
+const sorted = colors.toSorted((a, b) => {
+    if (a > b) return -1;
+    if (a < b) return 1;
+    return 0;
+});
+console.log(colors);    // [ 'blue', 'white', 'yellow', 'black', 'blue' ]
+console.log(sorted);    // [ 'yellow', 'white', 'blue', 'blue', 'black' ]
+
+console.log(colors.toSorted()); // [ 'black', 'blue', 'blue', 'white', 'yellow' ]
+
+// 非数组排序
+console.log(Array.prototype.toSorted.call(names));  // [ 'jack', 'jim', 'joe', 'lily', 'rose', 'tom' ]
+
+// 稀疏数组排序
+console.log(["jack", , "tom", "",,].toSorted());    // [ '', 'jack', 'tom', undefined, undefined ]
+
+
+// ------------------------------------------------------------------------------
+// 数组遍历：forEach()
+// ------------------------------------------------------------------------------
+colors = ["blue", "white", "yellow", "black", "blue"];
+colors.forEach((value, index, arrays) => {
+    console.log({value, index, arrays});
+});
+
+
+// ------------------------------------------------------------------------------
+// 数组合并：concat()
+// ------------------------------------------------------------------------------
+colors = ["blue", "white", "yellow", "black", "blue"];
+console.log(colors.concat("Xxx"));  // [ 'blue', 'white', 'yellow', 'black', 'blue', 'Xxx' ]
+console.log(colors.concat(["Xxx", "#red", "#yellow"])); // [ 'blue', 'white', 'yellow', 'black', 'blue', 'Xxx', '#red', '#yellow' ]
+
+// 合并后返回新数组
+console.log(colors.concat(...["Xxx1", "#red2", "#yellow3"])); // [ 'blue', 'white', 'yellow', 'black', 'blue', 'Xxx1', '#red2', '#yellow3' ]
+console.log(colors.concat("#CCC", "#ddd")); // [ 'blue', 'white', 'yellow', 'black', 'blue', '#CCC', '#ddd' ]
+
+// 合并多个数组
+const odds = [1, 3, 5];
+const evens = [2, 4, 6];
+console.log([].concat(odds, evens));    // [ 1, 3, 5, 2, 4, 6 ]
+
+
+// ------------------------------------------------------------------------------
+// 创建数组：of()
+// ------------------------------------------------------------------------------
+console.log(Array.of(7));       // [ 7 ]
+console.log(Array.of(1, 2, 3));  // [ 1, 2, 3 ]
+
+
+// ------------------------------------------------------------------------------
+// 数组排序： toReversed() 反转数组，但不更改原始数组
+// ----------
